5b25d833673369d06288c5ca8c8fb2530fe34a13
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-05-11 17:09:11 +0100

    Camera: add camera zoom plugin
    - fix gimbal test world
    - add example world for gimbal and camera
    - add gimbal_small_3d
    - update command topic names in gimbal test world
    - update quote style in gimbal_small_3d
    - rename zoom plugin and attach to sensor
    - resolve parent model and subscribe to zoom command
    - implement zoom for standard camera
    - remove whitespace for linter
    - provide alternative to gz::sim::Sensor
    - handle RenderTeardown event to exit cleanly
    - find package gz-common5
    - use degrees for pose in gimbal world
    - format plugin attributes.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ include/CameraZoomPlugin.hh
@@ -0,0 +1,67 @@
+/*
+   Copyright (C) 2023 ArduPilot.org
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef CAMERAZOOMPLUGIN_HH_
+#define CAMERAZOOMPLUGIN_HH_
+
+#include <memory>
+
+#include <gz/sim/System.hh>
+
+namespace gz {
+namespace sim {
+inline namespace GZ_SIM_VERSION_NAMESPACE {
+namespace systems {
+
+/// \brief Camera zoom plugin.
+class CameraZoomPlugin :
+    public System,
+    public ISystemConfigure,
+    public ISystemPreUpdate,
+    public ISystemPostUpdate
+{
+  /// \brief Destructor
+  public: virtual ~CameraZoomPlugin();
+
+  /// \brief Constructor
+  public: CameraZoomPlugin();
+
+  // Documentation inherited
+  public: void PreUpdate(const gz::sim::UpdateInfo &_info,
+                         gz::sim::EntityComponentManager &_ecm) final;
+
+  // Documentation inherited
+  public: void PostUpdate(const gz::sim::UpdateInfo &_info,
+                         const gz::sim::EntityComponentManager &_ecm) final;
+
+  // Documentation inherited
+  public: void Configure(const Entity &_entity,
+                         const std::shared_ptr<const sdf::Element> &_sdf,
+                         EntityComponentManager &_ecm,
+                         EventManager &) final;
+
+  /// \internal
+  /// \brief Private implementation
+  private: class Impl;
+  private: std::unique_ptr<Impl> impl;
+};
+
+}  // namespace systems
+}
+}  // namespace sim
+}  // namespace gz
+
+#endif  // CAMERAZOOMPLUGIN_HH_
